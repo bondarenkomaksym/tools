4785a909a1d1ba68b20c2d583d54f28265898cc8
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-09-14 15:13:19 +0300

    master

---
+++ les_3/task1/src/index.js
@@ -0,0 +1,17 @@
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+//input:
+//output:
+
+"use strict";
+
+const data = [1, 2, [3, 4, [5, 6]]];
+
+const result = data.flat(2);
+console.log(result);
+
+Promise.resolve().finally();
