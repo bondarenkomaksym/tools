7f6456e7e1a7566699a18b6e5ef15950a18e6810
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-09-15 20:27:40 +0300

    master

---
+++ les_6/task3/src/index.js
@@ -0,0 +1,22 @@
+import { initTodoListHandlers } from "./list/todoList.js";
+import { renderTasks } from "./list/render.js";
+import { getTasksList } from "./list/tasksGateway.js";
+import { setItem } from "./list/storage.js";
+import "./index.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+  getTasksList().then((tasksList) => {
+    setItem("tasksList", tasksList);
+    renderTasks();
+  });
+
+  initTodoListHandlers();
+});
+
+const onStorageChange = (e) => {
+  if (e.key === "tasksList") {
+    renderTasks();
+  }
+};
+
+window.addEventListener("storage", onStorageChange);
