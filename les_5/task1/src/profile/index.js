ecbfacb948f6065f299425c3d6f41014b7c13351
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-09-15 12:17:34 +0300

    master

---
+++ les_5/task1/src/profile/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// file path: src/profile/index.js
+
+export const printProfile = (profileData) => {
+  const { name, company } = profileData;
+  console.log(`${name} from ${company}`);
+};
